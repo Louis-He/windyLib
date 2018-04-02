4b0e39edee2a04b4b25de9efe2762a03a350cb9a
Louis-He <1726110778@qq.com>
2018-04-02 00:07:58 -0400

    update lib

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name = "windyLib",
     description = "weather data access and analysis from www.windy.com",
     author = "Louis He",
     author_email = "1726110778@qq.com",
-    url = "whatever",
+    url = "https://github.com/Louis-He/windyLib",
     packages= find_packages(),
     long_description = """weather data access and analysis from www.windy.com"""
 )
