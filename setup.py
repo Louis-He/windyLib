ce79cb369e8fd72b304234f7e5a41ef2579d835b
Louis-He <1726110778@qq.com>
2018-04-02 01:03:13 -0400

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name = "windyLib",
-    version = "0.1",
+    version = "0.1.1",
     description = "weather data access and analysis from www.windy.com",
     author = "Louis He",
     author_email = "1726110778@qq.com",
