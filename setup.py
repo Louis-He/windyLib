4ce8d07d18941988069afebcd3e6189819e16450
Louis-He <1726110778@qq.com>
2018-04-02 00:08:48 -0400

    update

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
 from setuptools import setup, find_packages
 
-#This is a list of files to install, and where
-#(relative to the 'root' dir, where setup.py is)
-#You could be more specific.
-files = ["things/*"]
-
 setup(name = "windyLib",
     version = "0.1",
     description = "weather data access and analysis from www.windy.com",
